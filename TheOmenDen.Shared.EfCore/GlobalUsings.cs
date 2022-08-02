9a0a286100528f9d174e601332282ec5a1ce853f
Andrew Diego Beers <andrew.d.beers@gmail.com>
2022-08-02 02:06:07 -0400

    Add project files.

---
+++ TheOmenDen.Shared.EfCore/GlobalUsings.cs
@@ -0,0 +1,14 @@
+// Global using directives
+#region System Usings
+global using System.Data.Common;
+global using System.Linq.Expressions;
+#endregion
+#region Microsoft Usings
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using Microsoft.EntityFrameworkCore.Storage;
+#endregion
+#region TheOmenDen Usings
+global using TheOmenDen.Shared.Enumerations;
+global using TheOmenDen.Shared.Extensions;
+#endregion
---
+++ TheOmenDen.Shared.EfCore/Translators/BetweenToSql.cs
@@ -0,0 +1,32 @@
+﻿namespace TheOmenDen.Shared.EfCore.Translators;
+
+
+internal sealed class BetweenToSql
+{
+    // C# => value.IsBetween(lowerBound, upperBound)
+    // Sql => Where value Between lowerBound and upperBound
+
+    // C# => value.IsBetweenExclusiveBounds(lowerBound, upperBound)
+    // Sql => Where value is < upperBound && value > lowerBound
+
+
+    // dbSet.Where(c => c.IsBetween(1,5));
+    // dbSet.IsBetween(c => 1, c=> 5).Where(c => c.Name == name);
+    [DbFunction(name: "Between", schema: "dbo")]
+    public bool Between<T>(T value, Func<T, bool> betweenFunc)
+    where T : IComparable<T>
+    {
+        var sb = StringBuilderPoolFactory<T>.Exists(nameof(T)) ?
+            StringBuilderPoolFactory<T>.Get(nameof(T))
+                : StringBuilderPoolFactory<T>.Create(nameof(T));
+
+        //var databaseName = GetDatabaseName();
+        //var schemaName = GetSchemaName();
+        //var tableName = GetTableName();
+
+
+        sb.AppendLine("");
+
+        return betweenFunc?.Invoke(value) ?? false;
+    }
+}
